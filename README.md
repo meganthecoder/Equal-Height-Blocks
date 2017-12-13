1b6e4debeb986e2f0c03aa5051a4a8aeaa080756
Megan Thomas <mnthomas15@gmail.com>
2017-12-12 23:01:53 -0500

    bootstrap v4 updates

--- README.md
+++ README.md
@@ -1,25 +1,31 @@
 # Equal Height Masonry Feed
 
-Bit of jQuery code that finds the height of elements per row under class="equal-heights-container" and sets each row to its unique tallest height. 
+This code sets block elements to equal heights unique to their row. I find myself needing this in feeds of content where I have a large number of elements that need to be listed together in a single container. 
 
-I find myself needing this in content feeds where I have a large number of elements that need to be listed together in a single container. 
-
-Requires block elements under ".equal-heights-container" to have a Bootstrap column class.
+It requires block elements under ".equal-heights-container" to have a Bootstrap column class and to include the class "block" on the element that the height will get set to. Updated for Bootstrap v4.
 
 
 ```
 <ul class="equal-heights-container row">
-	<li class="col-md-6">
-		<p>Your content here</p>
+	<li class="col-md-4 col-sm-6">
+		<div class="block" style="height: 250px;">
+			<p>Your content here</p>
+		</div>
 	</li>
-	<li class="col-md-6">
-		<p>This content is a lot longer than the other content, and its container would not normally be equal to the element sharing its row.</p>
+	<li class="col-md-4 col-sm-6">
+		<div class="block" style="height: 250px;">
+			<p>This content is a lot longer than the other content, and its container would not normally be equal to the element sharing its row.</p>
+		</div>
 	</li>
-	<li class="col-md-6">
-		<p>Your content here</p>
+	<li class="col-md-4 col-sm-6">
+		<div class="block" style="height: 150px;">
+			<p>Your content here</p>
+		</div>
 	</li>
-	<li class="col-md-6">
-		<p>Your content here</p>
+	<li class="col-md-4 col-sm-6">
+		<div class="block" style="height: 150px;">
+			<p>Your content here</p>
+		</div>
 	</li>
 </ul>
 ```
--- js/equal-height-masonry-feed.js
+++ js/equal-height-masonry-feed.js
@@ -7,38 +7,61 @@
  * Website:    	  meganthecoder.com
  */
 
-(function($){
-	$(document).ready(function() {
+$(function() {
 
-		// Find number of blocks per row and pixel of mobile step
+	// Find number of blocks per row and pixel of mobile step
+	function doEqualHeights() {
 		var class_list = $('.equal-heights-container > *:first-child').attr('class').split(/\s+/);
 		var mobile = 1200;
 		var blocks_per_row = 2;
+		var window_width = $(window).width();
+		var ceiling = 0;
+
+		// Find the largest breakpoint
+		$.each(class_list, function() {
+			var class_parts = this.split('-');
+		  	var grid = class_parts[1];
+		  	if ( grid == 'sm' && ceiling < 1 ) {
+				ceiling = 1;
+			} else if ( grid == 'md' && ceiling < 2 ) {
+				ceiling = 2;
+			} else if ( grid == 'lg' && ceiling < 3 ) {
+				ceiling = 3;
+			}
+		});
+
+		// Determine the number of blocks per row
 		$.each(class_list, function() {
 			var class_parts = this.split('-');
 		  	var grid = class_parts[1];
 		  	var size = class_parts[2];
 			if (class_parts[0] == 'col') {
-			    if (grid == 'xs') {
-			      mobile = 479;
-			    } else if (mobile > 479 && grid == 'sm') {
-			      mobile = 767;
-			    } else if (mobile > 767 && grid == 'md') {
-			      mobile = 991;
-			    }
-			    if (Math.floor(size) == size && $.isNumeric(size)) {
-			      blocks_per_row = 12 / size;
+			    if (Math.floor(grid) == grid && $.isNumeric(grid) && (window_width < 576 || ceiling == 0) ) {
+		      		mobile = 0;
+			    	blocks_per_row = 12 / grid;
+			    } else if (grid == 'sm' && window_width >= 576 && (window_width < 768 || ceiling == 1) ) {
+			        mobile = 575;
+				    if (Math.floor(size) == size && $.isNumeric(size)) {
+				      blocks_per_row = 12 / size;
+				    }
+			    } else if (grid == 'md' && window_width >= 768 && (window_width < 992 || ceiling == 2) ) {
+			        mobile = 767;
+			      	if (Math.floor(size) == size && $.isNumeric(size)) {
+				      blocks_per_row = 12 / size;
+				    }
+			    } else if (grid == 'lg' && window_width >= 992 ) {
+			        mobile = 991;
+			      	if (Math.floor(size) == size && $.isNumeric(size)) {
+				      blocks_per_row = 12 / size;
+				    }
 			    }
 			}
 		});
-
 		// Do the Equal Heights Computing
-		var window_width = $(window).width();
 		if ( $(".equal-heights-container").length > 0 && window_width > mobile) {
 			var tallest_by_row = [0];
 			var this_height = 0;
 			var current_row = 0;
-
 			// Find the tallest block height for each row
 			$('.equal-heights-container .block').each(function(index) {
 			    this_height = $(this).height();
@@ -65,6 +88,8 @@
 			    }
 			});
 		}
+	}
+	doEqualHeights();
 
-	});
-})(jQuery);
+
+});
